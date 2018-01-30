a0b2729d177cc15a29f5e27af6f124129ba2569e
Ben Williams <ben@negativeeddy.com>
2018-01-29 22:38:33 -0600

    Add project files.

---
+++ BaseBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BaseBot.WebApiApplication" Language="C#" %>
---
+++ BaseBot/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+using System;
+using System.Configuration;
+using System.Web.Http;
+
+namespace BaseBot
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var uri = new Uri(ConfigurationManager.AppSettings["DocumentDbUrl"]);
+            var key = ConfigurationManager.AppSettings["DocumentDbKey"];
+            var store = new DocumentDbBotDataStore(uri, key);
+
+            Conversation.UpdateContainer(
+                        builder =>
+                        {
+                            builder.Register(c => store)
+                                .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                                .AsSelf()
+                                .SingleInstance();
+
+                            builder.Register(c => new CachingBotDataStore(store, CachingBotDataStoreConsistencyPolicy.ETagBasedConsistency))
+                                .As<IBotDataStore<BotData>>()
+                                .AsSelf()
+                                .InstancePerLifetimeScope();
+
+                        });
+        }
+    }
+}
