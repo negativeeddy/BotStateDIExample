9dd2417db63935c57420b6b639223418ed2b658b
Ben Williams <ben@negativeeddy.com>
2018-01-29 22:53:33 -0600

    updated nuget packages

--- BaseBot/Global.asax.cs
+++ BaseBot/Global.asax.cs
@@ -31,7 +31,6 @@ namespace BaseBot
                                 .As<IBotDataStore<BotData>>()
                                 .AsSelf()
                                 .InstancePerLifetimeScope();
-
                         });
         }
     }
